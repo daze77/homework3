42f7c747fb6aa647d4a0c07b3d013bf69447f681
daze_77 <daze77@gmail.com>
2021-01-24 23:55:45 -0500

    found my error and fixed it...its done the best I can do now

--- Develop/script3.js
+++ Develop/script3.js
@@ -1,113 +1,124 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-
-//generate the variables to be used within the password generator
-var a = ("abcdefghijklmnopqrstuvwxyz")
-var b = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-var c = ("!@#$%^&*()")
-var d = (1234567890)
-var baseCharSet = a
-
-
-//////////Testing variables are showing up properly
-console.log(a)
-console.log(b)
-console.log(c)
-console.log(d)
-console.log(baseCharSet)
-
-
-// Trigger password generation
+// Write password to the #password input
 function writePassword() {
-    var password = generatePassword();
-}
- 
-
-//generate user prompts for inputs to questions
-var passwordLength = parseInt(prompt("Please select the length of your password", "Password from 8 - 128 characters!"))
-
-if(passwordLength < 8 || passwordLength > 128){
-    var passwordLength = parseInt(prompt("Sorry your password doesn't meet the requireemnts", "Please select a passowrd from 8 to 128 characters"))
-}
-var upperLetters = confirm("password to include uppers?")
-var specialLetters = confirm("password to include special characters?")
-var numberstoo = confirm("password to include numbers?")
-var lowerLetters = true
-
-console.log(baseCharSet)
-console.log(newbaseCharSet)
+  var password = generatePassword();
+  // var passwordText = document.querySelector("#password");
 
 
-//Console.log to verify the input is being logged  
-console.log(passwordLength)
-console.log(upperLetters)
-console.log(specialLetters)
-console.log(numberstoo)
-
-// This code is to determine what string to use to create the password based on the boolean results of what the person entered on screen
-
-if(upperLetters===true && specialLetters===false && numberstoo===false){
-var newbaseCharSet =  baseCharSet.concat(b)
-console.log(newbaseCharSet)
 }
 
-else if(upperLetters===true && specialLetters===true && numberstoo=== false){
-var newbaseCharSet=baseCharSet.concat(b, c)
-console.log(newbaseCharSet)
-}
 
-else if(upperLetters===true && specialLetters===true && numberstoo=== true){
-newbaseCharSet=baseCharSet.concat(b, c, d)
-console.log(newbaseCharSet)
-}
-
-else if(upperLetters===true && specialLetters===false && numberstoo=== true){
-var newbaseCharSet=baseCharSet.concat(b, d)
-console.log(newbaseCharSet)
-}
-
-else if (upperLetters===false && specialLetters===true && numberstoo=== false){
-var newbaseCharSet=baseCharSet.concat(c)
-console.log(newbaseCharSet)
-}
-else if (upperLetters===false && specialLetters===false && numberstoo=== true){
-var newbaseCharSet=baseCharSet.concat(d)
-console.log(newbaseCharSet)
-}
-else { (newbaseCharSet=baseCharSet)
-console.log(newbaseCharSet)
-
-}
-//testing of if statements based on inputs
-console.log(baseCharSet)
-console.log(newbaseCharSet)
-
-
-
-
-//generate the password based on inputs
-
-function generatePassword(){
-
+  function generatePassword(){
+    
+   
+   
+      console.log("Ok ready")
+    //generate the variables to be used within the password generator
+    var a = ("abcdefghijklmnopqrstuvwxyz")
+    var b = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+    var c = ("!@#$%^&*()")
+    var d = (1234567890)
+    var baseCharSet = a
+    
+    
+    //////////Testing variables are showing up properly
+    console.log(a)
+    console.log(b)
+    console.log(c)
+    console.log(d)
+    console.log(baseCharSet)
+    
+    
+    //generate user prompts for inputs to questions
+    var passwordLength = parseInt(prompt("Please select the length of your password", "Password from 8 - 128 characters!"))
+    
+    if(passwordLength < 8 || passwordLength > 128){
+        var passwordLength = parseInt(prompt("Sorry your password doesn't meet the requireemnts", "Please select a passowrd from 8 to 128 characters"))
+    }
+    var upperLetters = confirm("password to include uppers?")
+    var specialLetters = confirm("password to include special characters?")
+    var numberstoo = confirm("password to include numbers?")
+    var lowerLetters = true
+    
+    console.log(baseCharSet)
     console.log(newbaseCharSet)
+    
+    
+    //Console.log to verify the input is being logged  
     console.log(passwordLength)
-    console.log(newbaseCharSet.length)
-    //produce password based on length requested by user
-    var password = ""
-    for (   var i=0; i<passwordLength; i++) {
-            var charPick = Math.floor(Math.random()*newbaseCharSet.length)
-            password += newbaseCharSet[charPick]
-    }        
-         console.log(charPick)
-         console.log(newbaseCharSet)
-         console.log(newbaseCharSet.length)
-         console.log(password)
-        //write password to html
-        
-         document.querySelector('#password').value = password
-         
-}
+    console.log(upperLetters)
+    console.log(specialLetters)
+    console.log(numberstoo)
+    
+    // This code is to determine what string to use to create the password based on the boolean results of what the person entered on screen
+    
+    if(upperLetters===true && specialLetters===false && numberstoo===false){
+    var newbaseCharSet =  baseCharSet.concat(b)
+    console.log(newbaseCharSet)
+    }
+    
+    else if(upperLetters===true && specialLetters===true && numberstoo=== false){
+    var newbaseCharSet=baseCharSet.concat(b, c)
+    console.log(newbaseCharSet)
+    }
+    
+    else if(upperLetters===true && specialLetters===true && numberstoo=== true){
+    newbaseCharSet=baseCharSet.concat(b, c, d)
+    console.log(newbaseCharSet)
+    }
+    
+    else if(upperLetters===true && specialLetters===false && numberstoo=== true){
+    var newbaseCharSet=baseCharSet.concat(b, d)
+    console.log(newbaseCharSet)
+    }
+    
+    else if (upperLetters===false && specialLetters===true && numberstoo=== false){
+    var newbaseCharSet=baseCharSet.concat(c)
+    console.log(newbaseCharSet)
+    }
+    else if (upperLetters===false && specialLetters===false && numberstoo=== true){
+    var newbaseCharSet=baseCharSet.concat(d)
+    console.log(newbaseCharSet)
+    }
+    else { (newbaseCharSet=baseCharSet)
+    console.log(newbaseCharSet)
+    
+    }
+    //testing of if statements based on inputs
+    console.log(baseCharSet)
+    console.log(newbaseCharSet)
+    passwordGO()
 
-//listen for generate password button click - when clicked run 
-generateBtn.addEventListener("click", writePassword());
+
+
+
+    function passwordGO() {
+   
+      console.log(newbaseCharSet)
+      console.log(passwordLength)
+      console.log(newbaseCharSet.length)
+      //produce password based on length requested by user
+      var password = ""
+      for (   var i=0; i<passwordLength; i++) {
+              var charPick = Math.floor(Math.random()*newbaseCharSet.length)
+              password += newbaseCharSet[charPick]
+      }        
+          console.log(charPick)
+          console.log(newbaseCharSet)
+          console.log(newbaseCharSet.length)
+          console.log(password)
+          //write password to html
+          var passwordText = document.querySelector("#password");
+          passwordText.value = password;
+
+          
+    }
+    
+    }
+   
+   
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword)
